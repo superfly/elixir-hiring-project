0964a5e2770d132de1bc2e0abd6a8a4f443a1801
Kurt <mrkurt@gmail.com>
2021-02-20 16:43:36 -0600

    Show presence by region

--- lib/live_view_counter_web/live/counter.ex
+++ lib/live_view_counter_web/live/counter.ex
@@ -1,5 +1,7 @@
 defmodule LiveViewCounterWeb.Counter do
   use Phoenix.LiveView
+
+  alias LiveViewCounterWeb.Router.Helpers, as: Routes
   alias LiveViewCounter.Count
   alias Phoenix.PubSub
   alias LiveViewCounter.Presence
@@ -10,14 +12,14 @@ defmodule LiveViewCounterWeb.Counter do
   def mount(_params, _session, socket) do
     PubSub.subscribe(LiveViewCounter.PubSub, @topic)
 
-    Presence.track(self(), @presence_topic, socket.id, %{})
+    region = System.get_env("FLY_REGION") || "<unknown>"
+    Presence.track(self(), @presence_topic, socket.id, %{region: region})
     LiveViewCounterWeb.Endpoint.subscribe(@presence_topic)
 
-    initial_present =
-      Presence.list(@presence_topic)
-      |> map_size
+    list = Presence.list(@presence_topic)
+    present = presence_by_region(list)
 
-    {:ok, assign(socket, val: Count.current(), present: initial_present) }
+    {:ok, assign(socket, val: Count.current(), present: present) }
   end
 
   def handle_event("inc", _, socket) do
@@ -40,9 +42,32 @@ defmodule LiveViewCounterWeb.Counter do
         %{event: "presence_diff", payload: %{joins: joins, leaves: leaves}},
         %{assigns: %{present: present}} = socket
       ) do
-    new_present = present + map_size(joins) - map_size(leaves)
+
+    adds = presence_by_region(joins)
+    subtracts = presence_by_region(leaves)
+
+    new_present = %{}
+    for {k,v} <- adds do
+      new_present = Map.put(new_present, k, (new_present[k] || 0) + length(v))
+    end
+    for {k,v} <- subtracts do
+      new_present = Map.put(new_present, k, (new_present[k] || 0) - length(v))
+    end
+
+    #new_present = present + map_size(joins) - map_size(leaves)
+
 
     {:noreply, assign(socket, :present, new_present)}
+  end
+
+  @spec presence_by_region(Enum) :: Map
+  def presence_by_region(list) do
+    list
+    # |> IO.inspect
+    |> Enum.flat_map(fn {_, %{metas: metas}} -> metas end)
+    |> Enum.filter(fn m -> m[:region] != nil end)
+    |> Enum.group_by(fn r -> Map.get(r, :region) end)
+    # |> IO.inspect
   end
 
   def render(assigns) do
@@ -51,7 +76,13 @@ defmodule LiveViewCounterWeb.Counter do
       <h1>The count is: <%= @val %></h1>
       <button phx-click="dec">-</button>
       <button phx-click="inc">+</button>
-      <h1>Current users: <%= @present %></h1>
+      <h1>Current users</h1>
+      <%= for {k,v} <- @present do %>
+      <h2>
+      <span class="region">
+        <%= k %></span> <%= length(v) %>
+      </h2>
+      <% end %>
     </div>
     <div>
       Latency <span id="rtt" phx-hook="RTT" phx-update="ignore"></span>
@@ -59,3 +90,5 @@ defmodule LiveViewCounterWeb.Counter do
     """
   end
 end
+
+# <!--<img class="icon" src="<%= Routes.static_path(LiveViewCounterWeb.Endpoint, "/images/icons/#{k}.svg") %>" alt="<%= k %> flag"/>
